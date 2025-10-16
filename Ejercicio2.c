06c7994976dfba36fcba6fcbe37210e18f476ef9
CarlosVargas16 <carvargasr16@gmail.com>
2025-10-16 13:39:18 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,22 @@
+/*Carlos Antonio Vargas Rodriguez
+Ejercicio 2*/
+
+#include <stdio.h>
+
+int main()
+{
+   /* Al igual que en la estructura if-else, 0 -> falso y diferente de 0 -> verdadero.
+   El siguiente es un ciclo infinito porque la condición siempre es verdadera.
+   Así mismo, debido a que el ciclo consta de una sola línea, las llaves { } son
+   opcionales.*/
+
+int cont = 0
+ 
+while (cont <= 10)
+ {
+   printf("Ciclo infinito.\nPara terminar el ciclo presione ctrl + c.\n");
+  //cont = cont + 1;
+ }
+
+ return 0;
+}
